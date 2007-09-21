abca1a50e184b3e0f8e3bde8c9e061c7bdffa2d1
eknudsen <eknudsen@d066a591-ed20-0410-833c-cb8bbb91b4a7>
2007-09-21 09:41:34 +0000

    now sets bytecode as expected by fabian, and resets it when overflows are present - i.e. the image must be treated in 32 bits also removed some debugging prints
    git-svn-id: https://fable.svn.sourceforge.net/svnroot/fable/fabio/trunk@1566 d066a591-ed20-0410-833c-cb8bbb91b4a7

--- src/brukerimage.py
+++ src/brukerimage.py
@@ -15,38 +15,49 @@ Based on: openbruker,readbruker, readbrukerheader functions in the opendata
 
 import Numeric
 import math
+
+from fabio.fabioimage import fabioimage
+
 from PIL import Image
 import os
 
-class brukerimage:
-  data=None
-  header={}
-  dim1=dim2=0
-  m=maxval=stddev=minval=None
-  header_keys=[]
-  bytecode=None
+class brukerimage(fabioimage):
   
-  def toPIL16(self,filename=None):
-    if filename:
-      self.read(filename)
-      PILimage=Image.frombuffer("F",(self.dim1,self.dim2),self.data,"raw","F;16",0,-1)
-      return PILimage
-    else:
-      PILimage= Image.frombuffer("F",(self.dim1,self.dim2),self.data,"raw","F;16",0,-1)
-      return PILimage
+#this is now dispatched in fabioimage
+#  def toPIL16(self,filename=None):
+#    if filename:
+#      self.read(filename)
+#      PILimage=Image.frombuffer("F",(self.dim1,self.dim2),self.data,"raw","F;16",0,-1)
+#      return PILimage
+#    else:
+#      PILimage= Image.frombuffer("F",(self.dim1,self.dim2),self.data,"raw","F;16",0,-1)
+#      return PILimage
+
+  def readheader(self,f):
+    f=self._open(f)
+    self._readheader(f)
+    f.close()
 
   def _readheader(self,f):
+    """
+    the bruker format uses 80 char lines in key : value format 
+    In the fisrt 512*5 bytes of the header there should be a HDRBLKS key, whose value
+    denotes how many 512 byte blocks are in the total header. The header is always n*5*512 bytes,
+    otherwise it wont contain whole key: value pairs
+    """
+    
     l=f.read(512)
     i = 80
     self.header = {}
     while i < 512:
-      key,val=l[i-80:i].split(":",1)   # uses 80 char lines in key : value format
+      key,val=l[i-80:i].split(":",1)
       key=key.strip()         # remove the whitespace (why?)
       val=val.strip()
       if self.header.has_key(key):             # append lines if key already there
 	self.header[key]=self.header[key]+'\n'+val
       else:
 	self.header[key]=val
+        self.header_keys.append(key)
       i=i+80                  # next 80 characters
 
     nhdrblks=int(self.header['HDRBLKS'])    # we must have read this in the first 512 bytes.
@@ -55,27 +66,31 @@ class brukerimage:
     l = l[i-80:512] + rest
     j=512*nhdrblks
     while i < j :
-      # print i,"*",block[i-80:i].strip(),"*"
       if l[i-80:i].find(":") > 0:          # as for first 512 bytes of header
 	key,val=l[i-80:i].split(":",1)
         key=key.strip()
         val=val.strip()
         if self.header.has_key(key):
-	  self.header[key]=self.header[key]+'\n'+val
+          self.header[key]=self.header[key]+'\n'+val
         else:
           self.header[key]=val
+          self.header_keys.append(key)
       i=i+80
     self.header['datastart']=f.tell()        # make a header item called "datastart"
+    #set the image dimensions
+    self.dim1   =int(self.header['NROWS'])
+    self.dim2   =int(self.header['NCOLS'])
+
 	
   def read(self,fname,verbose=0):
-    f=open(fname,"rb")
-    
+    f=self._open(fname,"rb")
     try:
       self._readheader(f)
     except:
       raise
-    rows   =int(self.header['NROWS'])
-    cols   =int(self.header['NCOLS'])
+    
+    rows=self.dim1
+    cols=self.dim2
     
     try:
       npixelb=int(self.header['NPIXELB'])   # you had to read the Bruker docs to know this!
@@ -84,11 +99,10 @@ class brukerimage:
       for c in self.header['NPIXELB']:
 	print "char:",c,ord(c)
       raise
-    # We are now at the start of the image - assuming readbrukerheader worked
+    
     size=rows*cols*npixelb
-    self.data=self.readbytestream(f,f.tell(),rows,cols,npixelb,datatype="int",signed='n',swap='n')
+    self.data=self._readbytestream(f,f.tell(),rows,cols,npixelb,datatype="int",signed='n',swap='n')
     no=int(self.header['NOVERFL'])        # now process the overflows
-    print no
     if no>0:   # Read in the overflows
         # need at least Int32 sized data I guess - can reach 2^21
         self.data=self.data.astype(Numeric.UInt32)
@@ -103,13 +117,10 @@ class brukerimage:
             self.data[c,r]=intensity
     f.close()
     
-    #now read the data into the array
-    (self.dim1,self.dim2)=(rows,cols)
-    print self.dim1, self.dim2
     self.resetvals()
     return self
 
-  def readbytestream(self,file,offset,x,y,nbytespp,datatype='int',signed='n',
+  def _readbytestream(self,file,offset,x,y,nbytespp,datatype='int',signed='n',
 		swap='n',typeout=Numeric.UInt16):
     """
     Reads in a bytestream from a file (which may be a string indicating
@@ -128,7 +139,7 @@ class brukerimage:
     PLEASE LEAVE THE STRANGE INTERFACE ALONE - IT IS USEFUL FOR THE BRUKER FORMAT
     """
     tin="dunno"
-    print datatype
+    #print datatype
     len=nbytespp*x*y # bytes per pixel times number of pixels
     if datatype=='int' and signed=='n':
         if nbytespp==1 : tin=Numeric.UInt8
@@ -145,7 +156,7 @@ class brukerimage:
     if tin=="dunno" :
         raise SyntaxError, "Did not understand what type to try to read"
     if type(tin) == type(file):  # Did we get a string or a file pointer?
-        f=open(file,'rb')
+        f=self._open(file,'rb')
         opened=1
     else:
         f=file
@@ -158,98 +169,100 @@ class brukerimage:
                                Numeric.fromstring(f.read(len),tin) ,(x,y)),typeout)
     return ar
 
-  def getheader(self):
-    if self.header=={}:
-      print "No file loaded"
-    return self.header
-  
-  def getmax(self):
-    if self.maxval==None:
-      max_xel=Numeric.argmax(Numeric.ravel(self.data))
-      self.maxval=Numeric.ravel(self.data)[max_xel]
-    return int(self.maxval)
-  
-  def getmin(self):
-    if self.minval==None:
-      min_xel=Numeric.argmin(Numeric.ravel(self.data))
-      self.minval=Numeric.ravel(self.data)[min_xel]
-    return int(self.minval)
-
-
-  def integrate_area(self,coords,floor=0):
-    if not self.data:
-      return 0
-    else:
-      if coords[0]>coords[2]:
-	coords[0:3:2]=[coords[2],coords[0]]
-      if coords[1]>coords[3]:
-	coords[1:4:2]=[coords[3],coords[1]]
-      S=Numeric.sum(Numeric.ravel(self.data[int(coords[0]):int(coords[2])+1,int(coords[1]):int(coords[3])+1]))
-      S=S-floor*(1+coords[2]-coords[0])*(1+coords[3]-coords[1])
-    return S
-
-  def getmean(self):
-    if self.m==None:
-      self.m=Numeric.sum(Numeric.ravel(self.data.astype(Numeric.Float)))/(self.dim1*self.dim2)
-    return float(self.m)
-    
-  def getstddev(self):
-    if self.m==None:
-      self.getmean()
-      print "recalc mean"
-    if self.stddev==None:
-      N=self.dim1*self.dim2-1
-      S=Numeric.sum(Numeric.ravel((self.data.astype(Numeric.Float)-self.m)/N*(self.data.astype(Numeric.Float)-self.m)) )
-      self.stddev=S/(self.dim1*self.dim2-1)
-    return float(self.stddev)
-
-  def getheader(self):
-    return self.header
-
-  def add(self, otherImage):
-    if not hasattr(otherImage,'data'):
-      print 'brukerimage.add() called with something that does not have a data field'
-    try:
-      self.data=Numeric.clip(self.data+otherImage.data,0,65535)
-    except:
-      message='incompatible images - Do they have the same size?'
-      
-  def resetvals(self):
-    self.m=self.stddev=self.maxval=self.minval=None
-  
-  def rebin(self,x_rebin_fact,y_rebin_fact):
-    #not in working order yet
-    if self.data==None:
-      print 'Please read the file you wish to rebin first'
-      return
-    (mx,ex)=math.frexp(x_rebin_fact)
-    (my,ey)=math.frexp(y_rebin_fact)
-    if (mx!=0.5 or my!=0.5):
-      print 'Rebin factors not power of 2 not supported (yet)'
-      return
-    if int(self.dim1/x_rebin_fact)*x_rebin_fact!=self.dim1 or int(self.dim2/x_rebin_fact)*x_rebin_fact!=self.dim2:
-      print 'image size is not divisible by rebin factor - skipping rebin'
-      return
-    self.data.savespace(1)#avoid the upcasting behaviour
-    i=1
-    while i<x_rebin_fact:
-      self.data=((self.data[:,::2]+self.data[:,1::2])/2)
-      i=i*2
-    i=1
-    while i<y_rebin_fact:
-      self.data=((self.data[::2,:]+self.data[1::2,:])/2)
-      i=i*2
-    self.resetvals()
-    self.dim1=self.dim1/x_rebin_fact
-    self.dim2=self.dim2/y_rebin_fact
-    #update header
-    self.header['Dim_1']=self.dim1
-    self.header['Dim_2']=self.dim2
-    self.header['col_end']=self.dim1-1
-    self.header['row_end']=self.dim2-1
+#this is now taken care of in fabioimage
+#  def getheader(self):
+#    if self.header=={}:
+#      print "No file loaded"
+#    return self.header
+#  
+#  def getmax(self):
+#    if self.maxval==None:
+#      max_xel=Numeric.argmax(Numeric.ravel(self.data))
+#      self.maxval=Numeric.ravel(self.data)[max_xel]
+#    return int(self.maxval)
+#  
+#  def getmin(self):
+#    if self.minval==None:
+#      min_xel=Numeric.argmin(Numeric.ravel(self.data))
+#      self.minval=Numeric.ravel(self.data)[min_xel]
+#    return int(self.minval)
+#
+#
+#  def integrate_area(self,coords,floor=0):
+#    if not self.data:
+#      return 0
+#    else:
+#      if coords[0]>coords[2]:
+#	coords[0:3:2]=[coords[2],coords[0]]
+#      if coords[1]>coords[3]:
+#	coords[1:4:2]=[coords[3],coords[1]]
+#      S=Numeric.sum(Numeric.ravel(self.data[int(coords[0]):int(coords[2])+1,int(coords[1]):int(coords[3])+1]))
+#      S=S-floor*(1+coords[2]-coords[0])*(1+coords[3]-coords[1])
+#    return S
+#
+#  def getmean(self):
+#    if self.m==None:
+#      self.m=Numeric.sum(Numeric.ravel(self.data.astype(Numeric.Float)))/(self.dim1*self.dim2)
+#    return float(self.m)
+#    
+#  def getstddev(self):
+#    if self.m==None:
+#      self.getmean()
+#      print "recalc mean"
+#    if self.stddev==None:
+#      N=self.dim1*self.dim2-1
+#      S=Numeric.sum(Numeric.ravel((self.data.astype(Numeric.Float)-self.m)/N*(self.data.astype(Numeric.Float)-self.m)) )
+#      self.stddev=S/(self.dim1*self.dim2-1)
+#    return float(self.stddev)
+#
+#  def getheader(self):
+#    return self.header
+#
+#  def add(self, otherImage):
+#    if not hasattr(otherImage,'data'):
+#      print 'brukerimage.add() called with something that does not have a data field'
+#    try:
+#      self.data=Numeric.clip(self.data+otherImage.data,0,65535)
+#    except:
+#      message='incompatible images - Do they have the same size?'
+#      
+#  def resetvals(self):
+#    self.m=self.stddev=self.maxval=self.minval=None
+#  
+#  def rebin(self,x_rebin_fact,y_rebin_fact):
+#    #not in working order yet
+#    if self.data==None:
+#      print 'Please read the file you wish to rebin first'
+#      return
+#    (mx,ex)=math.frexp(x_rebin_fact)
+#    (my,ey)=math.frexp(y_rebin_fact)
+#    if (mx!=0.5 or my!=0.5):
+#      print 'Rebin factors not power of 2 not supported (yet)'
+#      return
+#    if int(self.dim1/x_rebin_fact)*x_rebin_fact!=self.dim1 or int(self.dim2/x_rebin_fact)*x_rebin_fact!=self.dim2:
+#      print 'image size is not divisible by rebin factor - skipping rebin'
+#      return
+#    self.data.savespace(1)#avoid the upcasting behaviour
+#    i=1
+#    while i<x_rebin_fact:
+#      self.data=((self.data[:,::2]+self.data[:,1::2])/2)
+#      i=i*2
+#    i=1
+#    while i<y_rebin_fact:
+#      self.data=((self.data[::2,:]+self.data[1::2,:])/2)
+#      i=i*2
+#    self.resetvals()
+#    self.dim1=self.dim1/x_rebin_fact
+#    self.dim2=self.dim2/y_rebin_fact
+#    #update header
+#    self.header['Dim_1']=self.dim1
+#    self.header['Dim_2']=self.dim2
+#    self.header['col_end']=self.dim1-1
+#    self.header['row_end']=self.dim2-1
   
   def write(self,fname):
-    f=open(fname,"wb")
+    print "***warning***: call to unifinished brukerimage.write. This will write the file", fname, "as an edf-file"
+    f=_open(fname,"wb")
     f.write('{\n')
     i=4
     for k in self.header_keys:
@@ -264,6 +277,9 @@ class brukerimage:
     else:
       f.write(self.data.byteswapped().astype(Numeric.UInt16).tostring())
     f.close()
+
+  def write2(self,fname):
+    pass 
 
 if __name__=='__main__':
   import sys,os,time
