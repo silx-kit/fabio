8e3a672bb36b8c0ab5d4b954ad66a9badc9169ab
eknudsen <eknudsen@d066a591-ed20-0410-833c-cb8bbb91b4a7>
2007-10-23 08:55:43 +0000

    switched to fabio and fabioimage corrected error (P3 was wrong -> should be P4) added stubs for RGB formats and P7 (pam) as well as a header reader for P7 (pam)
    git-svn-id: https://fable.svn.sourceforge.net/svnroot/fable/fabio/trunk@1774 d066a591-ed20-0410-833c-cb8bbb91b4a7

--- src/pnmimage.py
+++ src/pnmimage.py
@@ -14,29 +14,54 @@ Authors: Henning O. Sorensen & Erik Knudsen
 
 from PIL import Image
 import numpy.oldnumeric as Numeric
-import edfimage
+from fabio import fabioimage
 
-class pnmimage(edfimage.edfimage):
+SUBFORMATS=['P1','P2','P3','P4','P5','P6','P7']
+
+HEADERITEMS=['SUBFORMAT','DIMENSIONS','MAXVAL']
+P7HEADERITEMS=['WIDTH','HEIGHT','DEPTH','MAXVAL','TUPLTYPE','ENDHDR']
+
+class pnmimage(fabioimage.fabioimage):
     def __init__(self):
+        fun = getattr(fabioimage.fabioimage, '__init__', lambda x: None)
+        fun(self)
         self.data=None
         self.header={'Subformat':'P5'}
         self.dim1=self.dim2=0
         self.m=self.maxval=self.stddev=self.minval=None
         self.header_keys=self.header.keys()
         self.bytecode=None
-
-    def read(self,fname,verbose=0):
-        #read the (3 line header)
-        f=open(fname,"rb")
-
-        self.header_keys=['Subformat','Dimensions','Maxval']
+ 
+    def _readheader(self,f):
+        #pnm images have a 3-line header but ignore lines starting with '#'
         #1st line contains the pnm image sub format
         #2nd line contains the image pixel dimension
         #3rd line contains the maximum pixel value (at least for grayscale - check this)
-        for k in self.header_keys:
-            l=f.readline()
-            while(l[0]=='#'): l=f.readline()
-            self.header[k]=l.strip()
+        self.header_keys=['Subformat','Dimensions','Maxval']
+        
+        l=f.readline().strip()
+        if l not in SUBFORMATS:
+            raise IOError, ('unknown subformat of pnm: %s' % l)
+        else:
+            self.header['Subformat']=l
+            
+        if self.header['Subformat']=='P7':
+            self.header_keys=P7HEADERITEMS
+            #this one has a special header
+            while 'ENDHDR' not in l:
+                l=f.readline()
+                while(l[0]=='#'): l=f.readline()
+                s=l.lsplit(' ',1)
+                if s[0] not in P7HEADERITEMS:
+                    raise IOError, ('Illegal pam (netpnm p7) headeritem %s' % s[0])
+                self.self.header[s[0]]=s[1]
+        else:
+            self.header_keys=HEADERITEMS
+            for k in self.header_keys[1:]:
+                l=f.readline()
+                while(l[0]=='#'): l=f.readline()
+                self.header[k]=l.strip()
+
         #set the dimensions
         dims=(self.header['Dimensions'].split())
         self.dim1,self.dim2=int(dims[0]),int(dims[1])
@@ -52,20 +77,34 @@ class pnmimage(edfimage.edfimage):
             warn('32-bit pixels are not really supported by the netpgm standard')
         else:
             raise IOError, 'could not figure out what kind of pixels you have'
+
+    def read(self,fname,verbose=0):
+        self.header = {}
+        self.resetvals()
+        infile = self._open(fname)
+        self._readheader(infile)
+
         #read the image data
         try:
             #let the Subformat header field pick the right decoder
-            self.data=eval('self.'+self.header['Subformat']+'dec(f,self.bytecode)')
+            self.data=eval('self.'+self.header['Subformat']+'dec(infile,self.bytecode)')
         except ValueError:
             raise IOError
         self.resetvals()
         return self
 
     def P1dec(self,buf,bytecode):
-        warn('single bit images is not supported - yet')
+        data=Numeric.zeros((self.dim2,self.dim1))
+        i=0
+        for l in buf.readlines():
+            try:
+                data[i,:]=Numeric.array(l.split()).astype(bytecode)
+            except ValueError:
+                raise IOError, 'Size spec in pnm-header does not match size of image data field'
+        return data
 
-    def P3dec(self,buf,bytecode):
-        warn('single bit images is not supported - yet')
+    def P4dec(self,buf,bytecode):
+        warn('single bit (pbm) images are not supported - yet')
 
     def P2dec(self,buf,bytecode):
         data=Numeric.zeros((self.dim2,self.dim1))
@@ -84,6 +123,18 @@ class pnmimage(edfimage.edfimage):
         except ValueError:
             raise IOError, 'Size spec in pnm-header does not match size of image data field'
         return data
+
+    def P3dec(self,buf,bytecode):
+        #decode (plain-ppm) rgb image
+        pass
+
+    def P6dec(self,buf,bytecode):
+        #decode (ppm) rgb image
+        pass
+    
+    def P7dec(self,buf,bytecode):
+        #decode pam images, i.e. call one of the other decoders
+        pass
 
     def write(filename):
         warn('write pnm images is not implemented yet.')
