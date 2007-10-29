3f4f5d66656ebe4ca020d9abf747297fe57cff30
jpwright <jpwright@d066a591-ed20-0410-833c-cb8bbb91b4a7>
2007-10-29 11:17:28 +0000

    Update the script for picking up test images
    git-svn-id: https://fable.svn.sourceforge.net/svnroot/fable/fabio/trunk@1816 d066a591-ed20-0410-833c-cb8bbb91b4a7

--- test/get_test_images.sh
+++ test/get_test_images.sh
@@ -1,10 +1,31 @@
-#!/usr/bin/env sh
+#!/bin/sh
 
-#wget "http://teamsites.risoe.dk/totalcryst/File exchange/fabio_testimages.zip"
+
+#
+wget "http://teamsites.risoe.dk/totalcryst/File exchange/fabio_testimages.zip"
+# --http-user=username
+# --http-password=password
+
 cd testimages
-#unzip ../fabio_testimages.zip
+
+unzip ../fabio_testimages.zip
+
 for name in $(ls *.bz2 ) ; do
     bunzip2 -k $name
     gzip ${name%.bz2}
     bunzip2 -k $name
 done
+
+fit2d_12_081_i686_linux2.4.20 -nogr <<EOF
+3072
+3072
+NO
+INPUT
+ADSC
+mb_LP_1_001.img
+OUTPUT
+"KLORA" 
+mb_LP_1_001.edf
+QUIT
+YES
+EOF
