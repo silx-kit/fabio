8f90857f5828d197750bd448c62a872e05fcb505
jpwright <jpwright@d066a591-ed20-0410-833c-cb8bbb91b4a7>
2008-10-23 09:51:39 +0000

    Thomas Heller's libtiff3 binding via ctypes
    git-svn-id: https://fable.svn.sourceforge.net/svnroot/fable/fabio/trunk@3513 d066a591-ed20-0410-833c-cb8bbb91b4a7

---
+++ test/tiftest/testtif.py
@@ -0,0 +1,40 @@
+
+
+import glob, os, fabio.openimage, libtiff3ctypes
+
+
+fl = glob.glob( os.path.join("libtiffpic","*.tif"))
+fl += glob.glob( os.path.join("libtiffpic","depth","*.tif"))
+
+passed=0
+failed=0
+
+# Try with fabio
+for f in fl:
+    try:
+        im = fabio.openimage.openimage(f)
+        l = len(im.data.flat)
+        print "Passed for",f, im.data.shape, im.data.flat[l/2]
+        passed += 1
+    except:
+        print "Failed for",f
+        failed += 1
+
+print passed, failed, passed+failed
+passed=0
+failed=0
+
+# Now with libtiff
+for f in fl:
+    try:
+        tags, data = libtiff3ctypes.GetTiffTagsAndData(f)
+        l = len(data)
+        print "Passed for",f, l, data[l/2]
+        passed += 1
+    except:
+        print "Failed for",f
+        failed += 1
+
+
+
+print passed, failed, passed+failed
