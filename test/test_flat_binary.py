9facd1698a2cc5340a4002039cb38ba2202c85c0
jpwright <jpwright@d066a591-ed20-0410-833c-cb8bbb91b4a7>
2008-10-30 13:04:55 +0000

    Added several filenames
    git-svn-id: https://fable.svn.sourceforge.net/svnroot/fable/fabio/trunk@3539 d066a591-ed20-0410-833c-cb8bbb91b4a7

--- test/test_flat_binary.py
+++ test/test_flat_binary.py
@@ -3,18 +3,38 @@
 import unittest, os
 import fabio.openimage
 
+
 class test_flat_binary(unittest.TestCase):
-    filename = "binary_1234.dat"
+
+    filenames = [
+        "not.a.file",
+        "bad_news_1234",
+        "empty_files_suck_1234.edf",
+        "notRUBY_1234.dat"]
+    
     def setUp(self):
-        f = open(self.filename,"wb")
-        # A 2048 by 2048 blank image
-        f.write("\0x0"*2048*2048*2)
+        for filename in self.filenames:
+            f = open(filename,"wb")
+            # A 2048 by 2048 blank image
+            f.write("\0x0"*2048*2048*2)
         f.close()
+        
     def test_openimage(self):
-        im = fabio.openimage.openimage( self.filename )
-        assert im.data.tostring() == "\0x0"*2048*2048*2, "Failed"
+        nfail = 0
+        for filename in self.filenames:
+            try:
+                im = fabio.openimage.openimage( filename )
+                if im.data.tostring() != "\0x0"*2048*2048*2:
+                    nfail += 1
+                else:
+                    print "**** Passed", filename
+            except:
+                print "failed for",filename
+                nfail += 1
+        assert nfail == 0, str(nfail)+" failures out of "+str(len(self.filenames))        
     def tearDown(self):
-        os.remove(self.filename)
+        for filename in self.filenames:
+            os.remove(filename)
 
 if __name__=="__main__":
     unittest.main()
