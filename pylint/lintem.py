31a9e906392c07a9c8582cf3cfc60b1e00947667
kieffer <kieffer@d066a591-ed20-0410-833c-cb8bbb91b4a7>
2011-01-24 11:40:02 +0000

    Updated version of pylint analysis of the code.
    git-svn-id: https://fable.svn.sourceforge.net/svnroot/fable/fabio/trunk@5130 d066a591-ed20-0410-833c-cb8bbb91b4a7

--- pylint/lintem.py
+++ pylint/lintem.py
@@ -5,17 +5,20 @@
 import os, subprocess, sys, distutils.util
 
 def lintit(infile, outfile):
-#    print infile, outfile
+    print ("Updating %s" % outfile)
     process = subprocess.Popen(["pylint", infile], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     rc = process.wait()
-    if rc == 0:
-        open(outfile, "wb").write(process.stdout.read())
+    result = process.stdout.read()
+    if len(result) > 0:
+        open(outfile, "wb").write(result)
     else:
         print ("Error in running pylint on %s" % infile)
 
 arch = "lib.%s-%i.%i" % (distutils.util.get_platform(), sys.version_info[0], sys.version_info[1])
 installDir = os.path.abspath(os.path.join("..", "build", arch, "fabio"))
 testDir = os.path.abspath(os.path.join("..", "test"))
+
+sys.path.append(installDir)
 files = [ os.path.join(installDir, i) for i in os.listdir(installDir) if i.endswith(".py") ] + \
         [ os.path.join(testDir, i) for i in os.listdir(testDir) if i.endswith(".py") ]
 
@@ -26,4 +29,6 @@ for f in files:
         lintit(f, outf)
     elif os.stat(f).st_mtime > os.stat(outf).st_mtime:
         lintit(f, outf)
-    lintit(f, outf)
+    else:
+        print ("Not updating %s" % outf)
+#    lintit(f, outf)
