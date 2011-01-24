734681b3870b93d608e5e263cbf18576c09019bd
kieffer <kieffer@d066a591-ed20-0410-833c-cb8bbb91b4a7>
2011-01-24 16:19:33 +0000

    updated for rev 0.0.6
    git-svn-id: https://fable.svn.sourceforge.net/svnroot/fable/fabio/trunk@5140 d066a591-ed20-0410-833c-cb8bbb91b4a7

--- pydoc/makedocs.py
+++ pydoc/makedocs.py
@@ -1,13 +1,12 @@
-
-
+#!/usr/bin/env python
 
 import pydoc, os, fabio, inspect, shutil
 pydoc.writedoc("fabio")
-shutil.copyfile("fabio.html","index.html")
-done=[None]
+shutil.copyfile("fabio.html", "index.html")
+done = [None]
 for file in os.listdir(fabio.__path__[0]):
     path = os.path.join(fabio.__path__[0], file)
     modname = inspect.getmodulename(file)
     if modname not in done: # and modname != '__init__':
-        pydoc.writedoc("fabio."+modname)
+        pydoc.writedoc("fabio." + modname)
         done.append(modname)
