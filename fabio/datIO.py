75d4391a86b468374d4f10cf8663decbdf90eee2
kieffer <kieffer@d066a591-ed20-0410-833c-cb8bbb91b4a7>
2011-03-16 14:41:31 +0000

    towards v0.0.7 ... minor clean up
    git-svn-id: https://fable.svn.sourceforge.net/svnroot/fable/fabio/trunk@5167 d066a591-ed20-0410-833c-cb8bbb91b4a7

--- fabio/datIO.py
+++ fabio/datIO.py
@@ -9,52 +9,52 @@ Authors: Henning O. Sorensen & Erik Knudsen
          
          and Jon Wright, ESRF
 """
-import numpy
+#import numpy
 
-class fabiodata:
-  """
-  A common class for dataIO in fable
-  Contains a 2d numpy array for keeping data, and two lists (clabels and rlabels)
-  containing labels for columns and rows respectively
-  """
+class fabiodata(object):
+    """
+    A common class for dataIO in fable
+    Contains a 2d numpy array for keeping data, and two lists (clabels and rlabels)
+    containing labels for columns and rows respectively
+    """
 
-  def __init__(self,data=None,clabels=None,rlabels=None,fname=None):
-    """
-    set up initial values
-    """
-    if type(data) == type("string"):
-            raise Exception("fabioimage.__init__ bad argument - "+\
-                            "data should be numpy array")
-    self.data=data
-    if (self.data):
-      self.dims=self.data.shape
-    self.clabels=clabels
-    self.rlabels=rlabels
-    if (fname):
-      self.read(fname)
+    def __init__(self, data=None, clabels=None, rlabels=None, fname=None):
+        """
+        set up initial values
+        """
+        if type(data) == type("string"):
+                raise Exception("fabioimage.__init__ bad argument - " + \
+                                "data should be numpy array")
+        self.data = data
+        if (self.data):
+            self.dims = self.data.shape
+        self.clabels = clabels
+        self.rlabels = rlabels
+        if (fname):
+            self.read(fname)
 
-  def read(self,fname=None):
-    """
-    To be overridden by format specific subclasses
-    """
-    raise Exception("Class has not implemented read method yet")
+    def read(self, fname=None):
+        """
+        To be overridden by format specific subclasses
+        """
+        raise Exception("Class has not implemented read method yet")
 
-  #import stuff from Jon's columnfile things
+#import stuff from Jon's columnfile things
 
 
 class columnfile (fabiodata):
- 
-  def read(self,fname):
-    import cf_io
-    try:
-      infile=open(fname,'rb')
-    except:
-      raise Exception("columnfile: file" + str(fname) + "not found.")
-    try:
-      (self.data,self.clabels)=cf_io.read(infile)
-    except:
-      raise Exception("columnfile: read error, file " + str(fname) + " possibly corrupt" )        
-    self.dims=self.data.shape
-    infile.close()
+
+    def read(self, fname):
+        import cf_io
+        try:
+            infile = open(fname, 'rb')
+        except:
+            raise Exception("columnfile: file" + str(fname) + "not found.")
+        try:
+            (self.data, self.clabels) = cf_io.read(infile)
+        except:
+            raise Exception("columnfile: read error, file " + str(fname) + " possibly corrupt")
+        self.dims = self.data.shape
+        infile.close()
 
 
