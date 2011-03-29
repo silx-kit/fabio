3db8afdca7b9f4432b864d5ee9f04b8738355312
h_osholm <h_osholm@d066a591-ed20-0410-833c-cb8bbb91b4a7>
2011-03-29 15:00:37 +0000

    added import of extract_filenumber from fabioutils
    git-svn-id: https://fable.svn.sourceforge.net/svnroot/fable/fabio/trunk@5176 d066a591-ed20-0410-833c-cb8bbb91b4a7

--- fabio/__init__.py
+++ fabio/__init__.py
@@ -3,6 +3,7 @@ version = "0.0.7"
 import fabioimage
 import openimage
 from fabioutils import filename_object, COMPRESSORS, jump_filename, \
-        previous_filename, next_filename, deconstruct_filename
+        previous_filename, next_filename, deconstruct_filename,\
+        extract_filenumber
 from openimage import openimage as open
 
