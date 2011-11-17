d08eab7eb9aae46d11b3c295a4e3eda3b1a87996
kieffer <kieffer@d066a591-ed20-0410-833c-cb8bbb91b4a7>
2011-11-17 10:47:31 +0000

    Update automatic-pylint scripts.
    git-svn-id: https://fable.svn.sourceforge.net/svnroot/fable/fabio/trunk@5243 d066a591-ed20-0410-833c-cb8bbb91b4a7

--- pylint/lintem.py
+++ pylint/lintem.py
@@ -1,29 +1,40 @@
 #!/usr/bin/python
+# coding: utf8
+from __future__ import with_statement
 
-
+__author__ = "Jerome Kieffer"
+__email__ = "jerome.kieffer@esrf.fr"
+__doc__ = "This runs pylint on all modules from fabio package"
+__date__ = "20111117"
+__status__ = "production"
+__licence__ = "GPL"
 
 import os, subprocess, sys, distutils.util
-
-def lintit(infile, outfile):
-    print ("Updating %s" % outfile)
-    process = subprocess.Popen(["pylint", infile], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    rc = process.wait()
-    result = process.stdout.read()
-    if len(result) > 0:
-        open(outfile, "wb").write(result)
-    else:
-        print ("Error in running pylint on %s" % infile)
+import numpy
+import Image
 
 arch = "lib.%s-%i.%i" % (distutils.util.get_platform(), sys.version_info[0], sys.version_info[1])
 installDir = os.path.abspath(os.path.join("..", "build", arch, "fabio"))
 testDir = os.path.abspath(os.path.join("..", "test"))
+sys.path.append(testDir)
+import utilstest
+env = {"PYTHONPAH":":".join([installDir, testDir, os.path.dirname(numpy.__file__), os.path.dirname(Image.__file__)])}
+def lintit(infile, outfile):
+    print ("Updating %s" % outfile)
+    process = subprocess.Popen(["pylint", infile], stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=env)
+    result, error = process.communicate()
+    if len(result) > 0:
+        with open(outfile, "wb") as lintfile:
+            if len(error) > 0:
+                lintfile.write(error + os.linesep)
+            lintfile.write(result)
+    else:
+        print ("Error in running pylint on %s:%s%s" % (infile, os.linesep, error))
 
-sys.path.append(installDir)
 files = [ os.path.join(installDir, i) for i in os.listdir(installDir) if i.endswith(".py") ] + \
         [ os.path.join(testDir, i) for i in os.listdir(testDir) if i.endswith(".py") ]
 
 for f in files:
-#    print f
     outf = os.path.split(f)[-1] + ".lint"
     if not os.path.exists(outf) :
         lintit(f, outf)
