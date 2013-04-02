2e880f30ed18127bde9b5b449682b3995369a8ac
kieffer <kieffer@d066a591-ed20-0410-833c-cb8bbb91b4a7>
2013-04-02 12:57:45 +0000

    PEP8: rename filename_objet -> FilenameObject, inherits from object update all other sub-module make deconstruct_filename a method of the object callable from constructor. deprecate the function (man warnings in tests)
    git-svn-id: https://fable.svn.sourceforge.net/svnroot/fable/fabio/trunk@5453 d066a591-ed20-0410-833c-cb8bbb91b4a7

--- fabio-src/__init__.py
+++ fabio-src/__init__.py
@@ -3,7 +3,7 @@ import logging
 logging.basicConfig()
 import fabioimage
 import openimage
-from fabioutils import filename_object, COMPRESSORS, jump_filename, \
+from fabioutils import COMPRESSORS, jump_filename, FilenameObject, \
         previous_filename, next_filename, deconstruct_filename, \
         extract_filenumber, getnum, construct_filename
 from openimage import openimage as open
