2f232191873bc401054e652f36e65b5975cad7e4
Jerome Kieffer <jerome.kieffer@esrf.fr>
2013-05-02 13:52:41 +0200

    bruker format arriving to production level

--- fabio-src/brukerimage.py
+++ fabio-src/brukerimage.py
@@ -19,12 +19,12 @@ Writer by Jérôme Kieffer, ESRF, Grenoble, France
 
 """
 __authors__ = ["Henning O. Sorensen" , "Erik Knudsen", "Jon Wright", "Jérôme Kieffer"]
-__date__ = "20130430"
+__date__ = "20130502"
 __status__ = "development"
 __copyright__ = "2007-2009 Risoe National Laboratory; 2010-2013 ESRF"
 __licence__ = "GPL"
 
-import numpy, logging
+import numpy, logging, sys
 from math import ceil
 import os, pwd, time
 logger = logging.getLogger("brukerimage")
@@ -32,15 +32,20 @@ from fabioimage import fabioimage
 from fabioutils import pad
 from readbytestream import readbytestream
 from types import StringTypes
+if sys.version_info[0] < 3:
+    bytes = str
 
 class brukerimage(fabioimage):
     """
     Read and eventually write ID11 bruker (eg smart6500) images
+
+    TODO: int32 -> float32 conversion according to the "linear" keyword 
+    
     """
+
 
     # needed if you feel like writing - see ImageD11/scripts/edf2bruker.py
 
-    __headerstring__ = ""
     SPACER = "\x1a\x04" #this is CTRL-Z CTRL-D
     HEADERS_KEYS = ["FORMAT",    #Frame format. Always “86” or "100" for Bruker-format frames.
                    "VERSION",   #Header version #, such as: 1 to 17 (6 is obsolete).
@@ -59,7 +64,7 @@ class brukerimage(fabioimage):
                    "MINIMUM",   #Minimum counts in a pixel (uncompressed value)
                    "MAXIMUM",   #Maximum counts in a pixel (uncompressed value)
                    "NONTIME",   #Number of on-time events
-                   "NLAT",      #Number of late events. Always zero for many detectors.
+                   "NLATE",     #Number of late events. Always zero for many detectors.
                    "FILENAM",   #(Original) frame filename
                    "CREATED",   #Date and time of creation
                    "CUMULAT",   #Accumulated frame exposure time in seconds
@@ -86,15 +91,18 @@ class brukerimage(fabioimage):
                    "CELL" ,     #Unit cell A,B,C,ALPHA,BETA,GAMMA
                    "MATRIX" ,   #9R Orientation matrix (P3 conventions)
                    "LOWTEMP",   #Low temp flag.
+                   "TEMP",      #set temperature
+                   "HITEMP",    #Acquired at high temperature
                    "ZOOM" ,     #Zoom: Xc, Yc, Mag used for HI-STAR detectors: 0.5 0.5 1.0
                    "CENTER" ,   #X, Y of direct beam at 2-theta = 0. These are raw center for raw frames and unwarped center for unwarped frames.
-                   "DISTANC"    #Sample-detector distance, cm (see CmToGrid value) Adds: Sample-detector grid/phosphor distance, cm
+                   "DISTANC",   #Sample-detector distance, cm (see CmToGrid value) Adds: Sample-detector grid/phosphor distance, cm
                    "TRAILER",   #Byte pointer to trailer info
                    "COMPRES",   #Compression scheme ID, if any. Such as: NONE, LINEAR (Linear scale, offset for pixel values, typically 1.0, 0.0).
+                   "LINEAR",    #Linear scale (1.0 0.0 for no change; 0.1 0 for divided by 10...)
                    "PHD" ,      # Discriminator: Pulse height settings. X100 and X1000 only. Stores CCD phosphor efficiency (first field).
                    "PREAMP" ,   #Preamp gain setting. X100 and X1000 only. SMART: Stores Roper CCD gain table index value.
                    "CORRECT",   #Flood table correction filename, UNKNOWN or LINEAR.
-                   "WARPFIL"    #Brass plate correction filename, UNKNOWN or LINEAR. Note: A filename here does NOT mean that spatial correction was performed. See TYPE and string “UNWARP” to determine that.
+                   "WARPFIL",   #Brass plate correction filename, UNKNOWN or LINEAR. Note: A filename here does NOT mean that spatial correction was performed. See TYPE and string “UNWARP” to determine that.
                    "WAVELEN",   #Wavelengths (average, a1, a2)
                    "MAXXY",     #X,Y pixel # of maximum counts (from lower corner of 0,0)
                    "AXIS",      #Scan axis ib Eulerian space (1-4 for 2-theta, omega, phi, chi) (0 =none, 2 = default).
@@ -103,6 +111,7 @@ class brukerimage(fabioimage):
                    "LUT",       #Recommended display lookup table
                    "DISPLIM",   #Recommended display limits
                    "PROGRAM",   #Name and version of program writing frame, such as:
+                   "ROTATE",    #Non zero if acquired by rotation of phi during scan (or oscilate)
                    "BITMASK",   #File name of active pixel mask associated with this frame or $NULL
                    "OCTMASK",   #Octagon mask parameters to use if BITMASK=$null. Min X, Min X+Y, Min Y, Max X-Y, Max X, Max X+Y, Max Y, Max Y-X.
                    "ESDCELL",   #Unit cell parameter standard deviations
@@ -114,8 +123,8 @@ class brukerimage(fabioimage):
                    "MORPH",     #Crystal morphology in CIFTAB string, such as “?”
                    "CCOLOR",    #Crystal color in CIFTAB string, such as “?”
                    "CSIZE",     #Crystal dimensions (3 ea) in CIFTAB string, such as “?”
-                   "DNSMET"     #Density measurement method in CIFTAB string, such as “?”
-                   "DARK"       #Name of dark current correction or NONE.
+                   "DNSMET",     #Density measurement method in CIFTAB string, such as “?”
+                   "DARK",       #Name of dark current correction or NONE.
                    "AUTORNG",   #Auto-ranging: gain, high-speed time, scale, offset, full linear scale Note: If full linear scale is zero, then CCDPARM full scale is the full linear scale (BIS frames).
                    "ZEROADJ",   #Goniometer zero corrections (refined in least squares)
                    "XTRANS",    #Crystal XYZ translations (refined in least squares)
@@ -131,52 +140,51 @@ class brukerimage(fabioimage):
         fabioimage.__init__(self, data, header)
         self.__bpp_file = None
         self.version = 86
+        self.__headerstring__ = ""
+
 
     def _readheader(self, infile):
         """
-        the bruker format uses 80 char lines in key : value format
-        In the fisrt 512*5 bytes of the header there should be a
+        The bruker format uses 80 char lines in key : value format
+        In the first 512*5 bytes of the header there should be a
         HDRBLKS key, whose value denotes how many 512 byte blocks
         are in the total header. The header is always n*5*512 bytes,
         otherwise it wont contain whole key: value pairs
         """
-        lump = infile.read(512 * 5)
-        self.__headerstring__ += lump
-        i = 80
+        line = 80
+        blocksize = 512
+        nhdrblks = 5 #by default we always read 5 blocks of 512
+        self.__headerstring__ = infile.read(blocksize * nhdrblks)
         self.header = {}
-        while i < 512 * 5:
-            if lump[i - 80: i].find(":") > 0:
-                key, val = lump[i - 80: i].split(":", 1)
+        for i in range(0, nhdrblks * blocksize, line):
+            if self.__headerstring__[i: i + line].find(":") > 0:
+                key, val = self.__headerstring__[i: i + line].split(":", 1)
                 key = key.strip()         # remove the whitespace (why?)
                 val = val.strip()
-                if self.header.has_key(key):
+                if key in self.header:
                     # append lines if key already there
-                    self.header[key] = self.header[key] + '\n' + val
+                    self.header[key] = self.header[key] + os.linesep + val
                 else:
                     self.header[key] = val
                     self.header_keys.append(key)
-            i = i + 80                  # next 80 characters
-        # we must have read this in the first 512 bytes.
+        # we must have read this in the first 5*512 bytes.
         nhdrblks = int(self.header['HDRBLKS'])
         # Now read in the rest of the header blocks, appending
-        rest = infile.read(512 * (nhdrblks - 5))
+        rest = infile.read(blocksize * (nhdrblks - 5))
         self.__headerstring__ += rest
-        lump = lump[i - 80: 512] + rest
-        i = 80
-        j = 512 * nhdrblks
-        while i < j :
-            if lump[i - 80: i].find(":") > 0: # as for first 512 bytes of header
-                key, val = lump[i - 80: i].split(":", 1)
+        for i in range(5 * blocksize, nhdrblks * blocksize, line):
+            if self.__headerstring__[i: i + line].find(":") > 0: # as for first 512 bytes of header
+                key, val = self.__headerstring__[i - 80: i].split(":", 1)
                 key = key.strip()
                 val = val.strip()
-                if self.header.has_key(key):
-                    self.header[key] = self.header[key] + '\n' + val
+                if key in self.header:
+                    self.header[key] = self.header[key] + os.linesep + val
                 else:
                     self.header[key] = val
                     self.header_keys.append(key)
             i = i + 80
         # make a (new) header item called "datastart"
-        self.header['datastart'] = infile.tell()
+        self.header['datastart'] = blocksize * nhdrblks
         #set the image dimensions
         self.dim1 = int(self.header['NROWS'])
         self.dim2 = int(self.header['NCOLS'])
@@ -285,22 +293,28 @@ class brukerimage(fabioimage):
         for key in self.HEADERS_KEYS:
             if key in self.header:
                 value = self.header[key]
+                line = key.ljust(7) + ":"
                 if type(value) in StringTypes:
                     if os.linesep in value:
-                        for i in value.split(os.linesep):
-                            headers.append((key.ljust(7) + ":%s" % i).ljust(80, " "))
+                        lines = value.split(os.linesep)
+                        for i in lines[:-1] :
+                            headers.append((line + bytes(i)).ljust(80, " "))
+                            line = key.ljust(7) + ":"
+                        line += bytes(lines[-1])
                     elif len(value) < 72:
-                        headers.append((key.ljust(7) + ":%s" % value).ljust(80, " "))
+                        line += bytes(value)
                     else:
-                        for i in range(len(value) / 72 + 1):
-                            headers.append((key.ljust(7) + ":%s" % value[72 * i:min(len(value), 72 + (i + 1))]).ljust(80, " "))
+                        for i in range(len(value) // 72):
+                            headers.append((line + bytes(value[72 * i:72 * (i + 1)])))
+                            line = key.ljust(7) + ":"
+                        line += value[72 * (i + 1):]
                 elif "__len__" in dir(value):
-                    if isinstance(value[0], float):
-                        headers.append((key.ljust(7) + ":%s" % " ".join(["%.3f" % i for i in value])).ljust(80, " "))
-                    else:
-                        headers.append((key.ljust(7) + ":%s" % " ".join([str(i) for i in value])).ljust(80, " "))
+                    f = "\%.%is" % 72 // len(value) - 1
+                    line += " ".join([f % i for i in value])
                 else:
-                    headers.append((key.ljust(7) + ":%s" % value).ljust(80, " "))
+                    line += bytes(value)
+                headers.append(line.ljust(80, " "))
+
         header = "".join(headers)
         if len(header) > 512 * self.header["HDRBLKS"]:
             tmp = ceil(len(header) / 512.0)
@@ -308,7 +322,8 @@ class brukerimage(fabioimage):
             for i in range(len(headers)):
                 if headers[i].startswith("HDRBLKS"):
                     headers[i] = headers.append(("HDRBLKS:%s" % self.header["HDRBLKS"]).ljust(80, " "))
-        return pad("".join(headers), self.SPACER + "."*78, 512 * int(self.header["HDRBLKS"]))
+        res = pad("".join(headers), self.SPACER + "."*78, 512 * int(self.header["HDRBLKS"]))
+        return res
 
     def gen_overflow(self):
         """
@@ -339,12 +354,12 @@ class brukerimage(fabioimage):
             self.header["CREATED"] = time.ctime()
         if not "NOVERFL" in self.header:
             self.header["NOVERFL"] = "0"
-        if not "NPIXELB" in self.header:
-            self.header["NPIXELB"] = self.calc_bpp()
-        if not "NROWS" in self.header:
-            self.header["NROWS"] = self.data.shape[0]
-        if not "NCOLS" in self.header:
-            self.header["NCOLS"] = self.data.shape[1]
+#        if not "NPIXELB" in self.header:
+        self.header["NPIXELB"] = self.calc_bpp()
+        #if not "NROWS" in self.header:
+        self.header["NROWS"] = self.data.shape[0]
+        #if not "NCOLS" in self.header:
+        self.header["NCOLS"] = self.data.shape[1]
         if not "WORDORD" in self.header:
             self.header["WORDORD"] = "0"
         if not "LONGORD" in self.header:
