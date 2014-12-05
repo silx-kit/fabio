262da33d4bbedce14a401c32d669ad3c19ec32d6
Jerome Kieffer <jerome.kieffer@esrf.fr>
2014-12-05 17:07:01 +0100

    Python compatibility for distribution

--- test/profile_compressed.py
+++ test/profile_compressed.py
@@ -2,31 +2,32 @@
 """
 Benchmark the bz2 and gzip modules compared to their system counterparts
 """
+from __future__ import print_function
 
 import cProfile, os, time, random, gzip, bz2, struct, sys
 
-try: 
+try:
     import pstats
 except:
-    print "Your package manager is probably teasing you"
-    print "try sudo apt-get install python-profiler"
+    print("Your package manager is probably teasing you")
+    print("try sudo apt-get install python-profiler")
     sys.exit()
 
 
 
-print "Setting up"
+print("Setting up")
 npts = int(1e6)
 data = [ random.random() for i in range(npts) ]
-sdata = struct.pack( "d"*npts, *tuple(data) )
-open("prof.dat","wb").write(sdata)
-open("prof2.dat","wb").write(" "*npts*8)
+sdata = struct.pack("d"*npts, *tuple(data))
+open("prof.dat", "wb").write(sdata)
+open("prof2.dat", "wb").write(" "*npts * 8)
 
 os.system("gzip -c prof.dat > prof.dat.gz")
 os.system("bzip2 -c prof.dat > prof.dat.bz2")
 os.system("gzip -c prof2.dat > prof2.dat.gz")
 os.system("bzip2 -c prof2.dat > prof2.dat.bz2")
 
-print "Done setup"
+print("Done setup")
 sys.stdout.flush()
 def tst(fobj, fname):
     """test"""
@@ -36,34 +37,34 @@ def tst(fobj, fname):
 
 
 
-print "Python gzip module"
+print("Python gzip module")
 start = time.time()
-cProfile.run( "tst(gzip.GzipFile, 'prof.dat.gz')", "gzstats")
+cProfile.run("tst(gzip.GzipFile, 'prof.dat.gz')", "gzstats")
 p = pstats.Stats("gzstats")
 p.strip_dirs().sort_stats(-1).print_stats()
 del p
 
 
-print "Python bz2 module"
-cProfile.run( "tst(bz2.BZ2File, 'prof.dat.bz2')", "bz2stats")
+print("Python bz2 module")
+cProfile.run("tst(bz2.BZ2File, 'prof.dat.bz2')", "bz2stats")
 p = pstats.Stats("bz2stats")
 p.strip_dirs().sort_stats(-1).print_stats()
 del p
 
 def tstsys(cmd):
     """ test system"""
-    fo = os.popen(cmd,"rb")
+    fo = os.popen(cmd, "rb")
     fo.read()
     return
 
-print "System gzip"
-cProfile.run( "tstsys('gzip -cd prof.dat.gz')", "gzosstats")
+print("System gzip")
+cProfile.run("tstsys('gzip -cd prof.dat.gz')", "gzosstats")
 p = pstats.Stats("gzosstats")
 p.strip_dirs().sort_stats(-1).print_stats()
 del p
 
-print "System bz2"
-cProfile.run( "tstsys('bzip2 -cd prof.dat.bz2')", "bz2osstats")
+print("System bz2")
+cProfile.run("tstsys('bzip2 -cd prof.dat.bz2')", "bz2osstats")
 p = pstats.Stats("bz2osstats")
 p.strip_dirs().sort_stats(-1).print_stats()
 del p
@@ -89,7 +90,7 @@ if sys.platform != "win32":
 
 for s in cl:
     t = timeit.Timer(s, setup="import os, gzip, bz2")
-    print s, ":", t.timeit(5)/5
+    print("%s\t:\t%s" % (s, t.timeit(5) / 5.))
 
 # Finally - shell version
 
@@ -97,35 +98,35 @@ if sys.platform == 'win32':
     start = time.time()
     s = "gzip -cd prof.dat.gz > junk"
     os.system(s)
-    print s, ":", time.time()-start, "seconds via shell"
+    print("%s\t:\t%s seconds via shell" % (s, time.time() - start))
     start = time.time()
-    s  = "bzip2 -cd prof.dat.bz2 > junk"
+    s = "bzip2 -cd prof.dat.bz2 > junk"
     os.system(s)
-    print s, ":", time.time()-start, "seconds via shell"
+    print("%s\t:\t%s seconds via shell" % (s, time.time() - start))
     start = time.time()
     s = "gzip -cd prof2.dat.gz > junk"
     os.system(s)
-    print s, ":", time.time()-start, "seconds via shell"
+    print("%s\t:\t%s seconds via shell" % (s, time.time() - start))
     start = time.time()
     s = "bzip2 -cd prof2.dat.bz2 > junk"
     os.system(s)
-    print s, ":", time.time()-start, "seconds via shell"
+    print("%s\t:\t%s seconds via shell" % (s, time.time() - start))
     os.remove("junk")
 else:
     sys.stdout.flush()
     s = "time gzip -cd prof.dat.gz > /dev/null"
-    print "Time shell gzip:", s
+    print("Time shell gzip: %s" % s)
     os.system(s)
     sys.stdout.flush()
     s = "time bzip2 -cd prof.dat.bz2 > /dev/null"
-    print "Time shell bzip2:", s
+    print("Time shell bzip2: %s" % s)
     os.system(s)
     sys.stdout.flush()
     s = "time gzip -cd prof2.dat.gz > /dev/null"
-    print "Time shell gzip:", s
+    print("Time shell gzip: %s" % s)
     os.system(s)
     sys.stdout.flush()
     s = "time bzip2 -cd prof2.dat.bz2 > /dev/null"
-    print "Time shell bzip2:", s
+    print("Time shell bzip2: %s" % s)
     os.system(s)
 
--- test/test_all_images.py
+++ test/test_all_images.py
@@ -2,19 +2,19 @@
 """
 Check we can read all the test images
 """
+from __future__ import print_function
 
-
-import glob, os, time, fabio.openimage, gzip, bz2,  pstats, sys
+import glob, os, time, fabio.openimage, gzip, bz2, pstats, sys
 try:
     import  cProfile
 except:
-    import  profile as cProfile 
+    import  profile as cProfile
 
 times = {}
 images = []
 
-for fname in glob.glob(os.path.join("testimages","*")):
-    if fname.find("header_only")==-1:
+for fname in glob.glob(os.path.join("testimages", "*")):
+    if fname.find("header_only") == -1:
         images.append(fname)
 
 images.sort()
@@ -24,73 +24,73 @@ def shellbench(cmd, imname):
     The shell appears to be lying about it's performance. It claims 
     zero time to gunzip a file when it actually takes 200 ms. This is 
     cheating via a cache I suspect. We shall try to avoid this problem
-    """ 
+    """
     if sys.platform != "win32":
-        os.system("touch "+imname)
+        os.system("touch " + imname)
     astart = time.time()
     dummy_file = os.popen(cmd + " " + imname, "rb").read()
     return time.time() - astart
-    
 
-print "I/O 1  : Time to read the image"
-print "I/O 2  : Time to read the image (repeat"
-print "Fabio  : Time for fabio to read the image"
-print "Shell  : Time for shell to do decompression"
-print "Python : Time for python to do decompression\n"
 
-print "I/O 1  I/O 2  Fabio  Shell  Python   Size/MB"
+print("I/O 1  : Time to read the image")
+print("I/O 2  : Time to read the image (repeat")
+print("Fabio  : Time for fabio to read the image")
+print("Shell  : Time for shell to do decompression")
+print("Python : Time for python to do decompression\n")
+
+print("I/O 1  I/O 2  Fabio  Shell  Python   Size/MB")
 for im in images:
     # Network/disk io time first
     start = time.clock()
-    the_file = open(im,"rb").read()
-    times[im] =  [ time.clock()-start ]
+    the_file = open(im, "rb").read()
+    times[im] = [ time.clock() - start ]
     start = time.clock()
     # Network/disk should be cached
-    the_file = open(im,"rb").read()
-    times[im].append( time.clock() - start )
+    the_file = open(im, "rb").read()
+    times[im].append(time.clock() - start)
     start = time.clock()
     try:
         fim = fabio.openimage.openimage(im)
     except KeyboardInterrupt:
         raise
     except:
-        print "Problem with", im
+        print("Problem with", im)
         continue
         # raise
-    times[im].append( time.clock() - start )
+    times[im].append(time.clock() - start)
     nt = 3
     ns = 2
-    # Now check for a fabio slowdown effect    
+    # Now check for a fabio slowdown effect
     if im[-3:] == '.gz':
         times[im].append(shellbench("gzip -cd ", im))
         nt += 1
         ns -= 1
         start = time.clock()
-        the_file = gzip.GzipFile(im,"rb").read()
-        times[im].append( time.clock()-start )  
+        the_file = gzip.GzipFile(im, "rb").read()
+        times[im].append(time.clock() - start)
         nt += 1
         ns -= 1
     if im[-4:] == '.bz2':
         times[im].append(shellbench("bzip2 -cd ", im))
-        nt += 1 
+        nt += 1
         ns -= 1
         start = time.clock()
-        the_file = bz2.BZ2File(im,"rb").read()
-        times[im].append( time.clock()-start )  
+        the_file = bz2.BZ2File(im, "rb").read()
+        times[im].append(time.clock() - start)
         nt += 1
         ns -= 1
     # Speed ratings in megabytes per second (for fabio)
     MB = len(the_file) / 1024.0 / 1024.0
     try:
-        print ("%.4f "*nt + " "*7*ns)% tuple(times[im]), "%8.3f"%(MB), im
+        print(("%.4f "*nt + " "*7 * ns) % tuple(times[im]), "%8.3f" % (MB), im)
     except:
-        print times[im], MB, im
+        print(times[im], MB, im)
         raise
 
-    cProfile.run("fabio.openimage.openimage(im)","stats")
+    cProfile.run("fabio.openimage.openimage(im)", "stats")
     p = pstats.Stats("stats")
-    # Hack around python2.4 
+    # Hack around python2.4
     s = sys.stdout
-    sys.stdout = open("profile.txt","a")
+    sys.stdout = open("profile.txt", "a")
     p.strip_dirs().sort_stats(-1).print_stats()
     sys.stdout = s
