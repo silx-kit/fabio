e840fa5ca5f6cf3a6bc38428e5f35e3ede6186bd
Jerome Kieffer <jerome.kieffer@esrf.fr>
2014-12-12 14:39:53 +0100

    builder for python 2+3

--- build-deb-python3.sh
+++ build-deb-python3.sh
@@ -5,7 +5,7 @@ cd dist
 tar -xzf  fabio-*.tar.gz
 cd fabio*
 export DEB_BUILD_OPTIONS=nocheck
-python3 setup.py --command-packages=stdeb.command sdist_dsc --with-python2=True --with-python3=True --no-python2-scripts=True bdist_deb
+python3 setup.py --command-packages=stdeb.command sdist_dsc --with-python2=True --with-python3=True --no-python3-scripts=True bdist_deb
 #python setup.py --command-packages=stdeb.command bdist_deb
 sudo dpkg -i deb_dist/python-fabio*.deb
 sudo dpkg -i deb_dist/python3-fabio*.deb
