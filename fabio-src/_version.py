62a6a0af3e8ad695fa0852428f62b34bbe67e328
Jerome Kieffer <jerome.kieffer@esrf.fr>
2015-01-23 10:41:06 +0100

    minor fixes

--- fabio-src/_version.py
+++ fabio-src/_version.py
@@ -1,6 +1,39 @@
+# coding: utf-8
+# Simple module to handle versions
 
-from collections import namedtuple
-_version_info = namedtuple("version_info", ["major", "minor", "micro", "releaselevel", "serial"])
+from __future__ import absolute_import, print_function, division
+
+__author__ = "Jerome Kieffer"
+__contact__ = "Jerome.Kieffer@ESRF.eu"
+__license__ = "GPLv3+"
+__copyright__ = "European Synchrotron Radiation Facility, Grenoble, France"
+__date__ = "23/01/2015"
+__status__ = "beta"
+__docformat__ = 'restructuredtext'
+__doc__ = """
+
+Module for version handling:
+
+provides:
+* version = "1.2.3" or "1.2.3-beta4"
+* version_info = named tuple (1,2,3,"beta",4)
+* hexversion: 0x010203B4
+
+This is called hexversion since it only really looks meaningful when viewed as the 
+result of passing it to the built-in hex() function. 
+The version_info value may be used for a more human-friendly encoding of the same information.
+
+The hexversion is a 32-bit number with the following layout:
+Bits (big endian order)     Meaning
+1-8     PY_MAJOR_VERSION (the 2 in 2.1.0a3)
+9-16     PY_MINOR_VERSION (the 1 in 2.1.0a3)
+17-24     PY_MICRO_VERSION (the 0 in 2.1.0a3)
+25-28     PY_RELEASE_LEVEL (0xA for alpha, 0xB for beta, 0xC for release candidate and 0xF for final)
+29-32     PY_RELEASE_SERIAL (the 3 in 2.1.0a3, zero for final releases)
+
+Thus 2.1.0a3 is hexversion 0x020100a3.
+
+"""
 
 MAJOR = 0
 MINOR = 2
@@ -8,23 +41,16 @@ MICRO = 1
 RELEV = "dev"  # <16
 SERIAL = 0  # <16
 
+
+from collections import namedtuple
+_version_info = namedtuple("version_info", ["major", "minor", "micro", "releaselevel", "serial"])
+
 version_info = _version_info(MAJOR, MINOR, MICRO, RELEV, SERIAL)
 
 version = "%d.%d.%d" % version_info[:3]
 if version_info.releaselevel != "final":
     version += "-%s%s" % version_info[-2:]
 
-# This is called hexversion since it only really looks meaningful when viewed as the result of passing it to the built-in hex() function. The version_info value may be used for a more human-friendly encoding of the same information.
-#
-# The hexversion is a 32-bit number with the following layout:
-# Bits (big endian order)     Meaning
-# 1-8     PY_MAJOR_VERSION (the 2 in 2.1.0a3)
-# 9-16     PY_MINOR_VERSION (the 1 in 2.1.0a3)
-# 17-24     PY_MICRO_VERSION (the 0 in 2.1.0a3)
-# 25-28     PY_RELEASE_LEVEL (0xA for alpha, 0xB for beta, 0xC for release candidate and 0xF for final)
-# 29-32     PY_RELEASE_SERIAL (the 3 in 2.1.0a3, zero for final releases)
-#
-# Thus 2.1.0a3 is hexversion 0x020100a3.
 RELEASE_LEVEL_VALUE = { "dev": 0, "alpha": 10, "beta": 11, "gamma": 11, "final":15}
 hexversion = version_info[4]
 hexversion |= RELEASE_LEVEL_VALUE.get(version_info[3], 0) * 1 << 4
--- setup.py
+++ setup.py
@@ -63,10 +63,13 @@ _cif_backend = Extension('_cif',
 
 extensions = [cf_backend, byteOffset_backend, mar345_backend, _cif_backend]
 
-sys.path.insert(0, op.join(op.dirname(op.abspath(__file__)), "fabio-src"))
-import _version
-sys.path.pop(0)
-version = _version.version
+
+def get_version():
+    sys.path.insert(0, op.join(op.dirname(op.abspath(__file__)), "fabio-src"))
+    import _version
+    sys.path.pop(0)
+    return _version.version
+
 #######################
 # build_doc commandes #
 #######################
@@ -257,7 +260,7 @@ else:
 
 if __name__ == "__main__":
     setup(name='fabio',
-          version=version,
+          version=get_version(),
           author="Henning Sorensen, Erik Knudsen, Jon Wright, Regis Perdreau, Jérôme Kieffer, Gael Goret, Brian Pauw",
           author_email="fable-talk@lists.sourceforge.net",
           description='Image IO for fable',
