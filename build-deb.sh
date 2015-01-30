4005ab708c2b36609d1eeed33ea972d8754e29bd
Jerome Kieffer <jerome.kieffer@esrf.fr>
2015-01-30 15:13:55 +0100

    enforce not checking at build time

--- build-deb.sh
+++ build-deb.sh
@@ -4,6 +4,7 @@ python setup.py sdist
 cd dist
 tar -xzf  fabio-*.tar.gz
 cd fabio*
+export DEB_BUILD_OPTIONS=nocheck
 python setup.py --command-packages=stdeb.command bdist_deb
 sudo dpkg -i deb_dist/python-fabio*.deb
 cd ../..
