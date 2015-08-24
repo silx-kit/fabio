621ba89ed95ae12acbdb23232e51ddbf5b604b5f
Jerome Kieffer <jerome.kieffer@esrf.fr>
2015-08-24 16:48:26 +0200

    print more info about compiler used and so on...

--- info_platform.py
+++ info_platform.py
@@ -3,6 +3,13 @@ import sys, numpy
 print("Python %s bits" % (tuple.__itemsize__ * 8))
 print("       maxsize: %s\t maxunicode: %s" % (sys.maxsize, sys.maxunicode))
 print(sys.version)
+try:
+    from distutils.sysconfig import get_config_vars
+except:
+    from sysconfig import get_config_vars
+for k, v in get_config_vars().items():
+    print("\t%s: %s" % (k, v))
+
 print("Numpy %s" % numpy.version.version)
 print("      include %s" % numpy.get_include())
 print("      options %s" % numpy.get_printoptions())
