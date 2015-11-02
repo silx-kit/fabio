bb8c3b5e413d55a38c03acc1170bc6b4acbeccca
Jerome Kieffer <jerome.kieffer@esrf.fr>
2015-11-02 10:44:49 +0100

    include test coverage into test running program

--- run_tests.py
+++ run_tests.py
@@ -1,38 +1,87 @@
 #!/usr/bin/env python
 import sys
+import os
+import logging
+
 print("Python %s %s" % (sys.version, tuple.__itemsize__ * 8))
 
-try: import numpy
-except: print("Numpy missing")
-else: print("Numpy %s" % numpy.version.version)
-
-
-
-try: import h5py
-except Exception as error: print("h5py missing: %s" % error)
-else: print("h5py %s" % h5py.version.version)
-
-try: import Cython
-except: print("Cython missing")
-else: print("Cython %s" % Cython.__version__)
-
-try: import fabio
+try:
+    import numpy
 except:
-    print("FabIO missing, using built version")
-    """Adapter for utilstest from pyFAI.test"""
-    import os
+    print("Numpy missing")
+else:
+    print("Numpy %s" % numpy.version.version)
+
+
+
+try:
+    import h5py
+except Exception as error: print("h5py missing: %s" % error)
+else:
+    print("h5py %s" % h5py.version.version)
+
+try:
+    import Cython
+except:
+    print("Cython missing")
+else:
+    print("Cython %s" % Cython.__version__)
+
+
+try:
+    from argparse import ArgumentParser
+except ImportError:
+    from fabio.third_party.argparse import ArgumentParser
+parser = ArgumentParser(description='Run the tests.')
+
+parser.add_argument("-i", "--insource",
+                    action="store_true", dest="insource", default=False,
+                    help="Use the build source and not the installed version")
+parser.add_argument("-c", "--coverage", dest="coverage",
+                    action="store_true", default=False,
+                    help="report coverage of fabio code (requires 'coverage' module)")
+parser.add_argument("-v", "--verbose",
+                    action="count", dest="verbose",
+                    help="increase verbosity")
+options = parser.parse_args()
+sys.argv = [sys.argv[0]]
+if options.verbose == 1:
+    logging.root.setLevel(logging.INFO)
+    print("Set log level: INFO")
+elif options.verbose > 1:
+    logging.root.setLevel(logging.DEBUG)
+    print("Set log level: DEBUG")
+
+if not options.insource:
+    try:
+        import fabio
+    except:
+        print("FabIO missing, using built (i.e. not installed) version")
+        options.insource = True
+if options.insource:
     home = os.path.abspath(__file__)
     sys.path.insert(0, home)
     from test.utilstest import *
-else:
-    print("FabIO %s" % fabio.version)
-    sys.path.append("sandbox")
+    import fabio
+
+print("FabIO %s from %s" % (fabio.version, fabio.__path__))
 
 
-import logging
-if "-v" in sys.argv:
-    logging.root.setLevel(logging.INFO)
-if "-d" in sys.argv:
-    logging.root.setLevel(logging.DEBUG)
+if options.coverage:
+    print("Running test-coverage")
+    import coverage
+    source = os.path.dirname(fabio.__file__)
+    try:
+        cov = coverage.Coverage(source=fabio.__path__, omit=["*test*", "*third_party*"])
+    except AttributeError:
+        cov = coverage.coverage(source=fabio.__path__, omit=["*test*", "*third_party*"])
+
+    cov.start()
 
 fabio.tests()
+
+if options.coverage:
+    cov.stop()
+    cov.save()
+    print(cov.report())
+
