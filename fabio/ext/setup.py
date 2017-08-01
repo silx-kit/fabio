b57b3582a72d30df27c27fd82a675219735ea6e5
Valentin Valls <valentin.valls@esrf.fr>
2017-08-01 10:38:37 +0200

    Fix numpy include

--- fabio/ext/setup.py
+++ fabio/ext/setup.py
@@ -24,10 +24,11 @@
 
 __authors__ = ["V. Valls"]
 __license__ = "MIT"
-__date__ = "31/07/2017"
+__date__ = "01/08/2017"
 
-from numpy.distutils.misc_util import Configuration
 import os
+import numpy
+from numpy.distutils.misc_util import Configuration
 
 
 def configuration(parent_package='', top_path=None):
@@ -36,23 +37,25 @@ def configuration(parent_package='', top_path=None):
     config.add_extension(
         name="cf_io",
         sources=["cf_io.pyx", os.path.join("src", "columnfile.c")],
-        include_dirs=["include"],
+        include_dirs=["include", numpy.get_include()],
         language='c')
 
     config.add_extension(
         name="byte_offset",
         sources=["byte_offset.pyx"],
+        include_dirs=[numpy.get_include()],
         language='c')
 
     config.add_extension(
         name="mar345_IO",
         sources=["mar345_IO.pyx", os.path.join("src", "ccp4_pack.c")],
-        include_dirs=["include"],
+        include_dirs=["include", numpy.get_include()],
         language='c')
 
     config.add_extension(
         name="_cif",
         sources=["_cif.pyx"],
+        include_dirs=[numpy.get_include()],
         language='c')
 
     return config
