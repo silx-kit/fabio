450c8193dbc6b782f22cea0b8d0eb151b73f6056
Jerome Kieffer <kieffer@lintaillefer.esrf.fr>
2018-01-16 14:17:47 +0100

    typo in setup.py

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ __author__ = "Jerome Kieffer"
 __contact__ = "Jerome.Kieffer@ESRF.eu"
 __license__ = "MIT"
 __copyright__ = "European Synchrotron Radiation Facility, Grenoble, France"
-__date__ = "15/01/2018"
+__date__ = "16/01/2018"
 __status__ = "stable"
 
 import sys
@@ -472,7 +472,7 @@ class Build(_build):
                 # By default Xcode5 & XCode6 do not support OpenMP, Xcode4 is OK.
                 osx = tuple([int(i) for i in platform.mac_ver()[0].split(".")])
                 if osx >= (10, 8):
-                    logger.warning("OpenMP support ignored. Your platform do not support it")
+                    logger.warning("OpenMP support ignored. Your platform does not support it.")
                     use_openmp = False
 
         # Remove attributes used by distutils parsing
