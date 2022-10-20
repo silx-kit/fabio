9e068886dc984a02cc80f14a239c33207e97b691
Jerome Kieffer <jerome.kieffer@esrf.fr>
2022-10-20 10:49:39 +0200

    make it compliant with pip naming

--- version.py
+++ version.py
@@ -90,7 +90,7 @@ version_info = _version_info(MAJOR, MINOR, MICRO, RELEV, SERIAL)
 strictversion = version = debianversion = "%d.%d.%d" % version_info[:3]
 if version_info.releaselevel != "final":
     _prerelease = PRERELEASE_NORMALIZED_NAME[version_info[3]]
-    version += "-%s%s" % (_prerelease, version_info[-1])
+    version += "%s%s" % (_prerelease, version_info[-1])
     debianversion += "~adev%i" % version_info[-1] if RELEV == "dev" else "~%s%i" % (_prerelease, version_info[-1])
     strictversion += _prerelease + str(version_info[-1])
 
