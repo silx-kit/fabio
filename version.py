eb296e8e1329ad5ed8c493af01458dda0d992a19
Jerome Kieffer <jerome.kieffer@esrf.fr>
2022-12-12 14:26:32 +0100

    switch to stable version

--- version.py
+++ version.py
@@ -78,8 +78,8 @@ PRERELEASE_NORMALIZED_NAME = {"dev": "a",
 MAJOR = 2022
 MINOR = 12
 MICRO = 0
-RELEV = "dev"  # <16
-SERIAL = 1  # <16
+RELEV = "final"  # <16
+SERIAL = 0  # <16
 date = __date__
 
 from collections import namedtuple
