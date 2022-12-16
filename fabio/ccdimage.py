4ee98ee1793b5b6487678b357cd9ef80ba0874e0
Jerome Kieffer <jerome.kieffer@esrf.fr>
2022-12-16 17:19:18 +0100

    Define the different data-structure stored in the CCD-file

--- fabio/ccdimage.py
+++ fabio/ccdimage.py
@@ -34,43 +34,201 @@ __authors__ = ["Jérôme Kieffer"]
 __contact__ = "jerome.kieffer@esrf.eu"
 __license__ = "MIT"
 __copyright__ = "2022 ESRF"
-__date__ = "15/12/2022"
+__date__ = "16/12/2022"
 
 import logging
 logger = logging.getLogger(__name__)
 import numpy
 from .fabioimage import FabioImage, OrderedDict
 from dataclasses import dataclass
-
+from enum import Enum
+import struct
 # Some constants used in the file:
 
-CCD_FILEVERSION_VERS_1_1 = 0x00010001
-CCD_FILEVERSION_VERS_1_2 = 0x00010002
-CCD_FILEVERSION_VERS_1_3 = 0x00010003
-CCD_FILEVERSION_VERS_1_4 = 0x00010004
-CCD_FILEVERSION_VERS_1_5 = 0x00010005
-CCD_FILEVERSION_VERS_1_6 = 0x00010006
-CCD_FILEVERSION_VERS_1_7 = 0x00010007
-CCD_FILEVERSION_VERS_1_8 = 0x00010008
-CCD_FILEVERSION_VERS_1_9 = 0x00010009
-CCD_FILEVERSION_VERS_1_10 = 0x0001000a
-CCD_FILEVERSION_VERS_1_11 = 0x0001000b
-CCD_FILEVERSION_VERS_1_12 = 0x0001000c
-CCD_FILEVERSION_VERS_HIGHEST =CCD_FILEVERSION_VERS_1_12
+class CCD_FILEVERSION(Enum):
+    VERS_1_1 = 0x00010001
+    VERS_1_2 = 0x00010002
+    VERS_1_3 = 0x00010003
+    VERS_1_4 = 0x00010004
+    VERS_1_5 = 0x00010005
+    VERS_1_6 = 0x00010006
+    VERS_1_7 = 0x00010007
+    VERS_1_8 = 0x00010008
+    VERS_1_9 = 0x00010009
+    VERS_1_10 = 0x0001000a
+    VERS_1_11 = 0x0001000b
+    VERS_1_12 = 0x0001000c
+    
+    
+CCD_FILEVERSION_VERS_HIGHEST =CCD_FILEVERSION.VERS_1_12
 
-CHIPCHARACTERISTICS_TREATMENT = {"IGNORE":            0,
-                                 "REPLACE":           1,
-#define CHIPCHARACTERISTICS_TREATMENT_AVERAGE            2
-#define CHIPCHARACTERISTICS_TREATMENT_REPLACELEFT        3
-#define CHIPCHARACTERISTICS_TREATMENT_REPLACERIGHT        4
-#define CHIPCHARACTERISTICS_TREATMENT_AVERAGELEFTRIGHT    5
-#define CHIPCHARACTERISTICS_TREATMENT_REPLACETOP        6
-#define CHIPCHARACTERISTICS_TREATMENT_REPLACEBOTTOM        7
-#define CHIPCHARACTERISTICS_TREATMENT_AVERAGETOPBOTTOM    8
-}
+class CHIPCHARACTERISTICS_TREATMENT(Enum):
+    IGNORE = 0
+    REPLACE = 1
+    AVERAGE = 2
+    REPLACELEFT = 3
+    REPLACERIGHT = 4
+    AVERAGELEFTRIGHT = 5
+    TREATMENT_REPLACETOP = 6
+    REPLACEBOTTOM = 7
+    AVERAGETOPBOTTOM = 8
+
+class CHIPCHARACTERISTICS_POLYGONTYPE(Enum):
+    TRIANGLE = 0
+    RECTANGLE = 1
+    POLYGON = 2
+    MAXPOINTS = 6
+
+class CHIPCHARACTERISTICS_SCINTILLATORID(Enum):
+    NOTPRESENT = 0
+    GREEN400 = 1
+    LEXEL40 = 2
+    LEXEL60 = 3
+    LEXEL100 = 4
+    RAREXMED = 5
+    RAREXFINE = 6
+    GREEN400_NEW = 7
+
+CHIPCHARACTERISTICS_SCINTILLATORID_FIRST = CHIPCHARACTERISTICS_SCINTILLATORID.GREEN400
+CHIPCHARACTERISTICS_SCINTILLATORID_LAST = CHIPCHARACTERISTICS_SCINTILLATORID.GREEN400_NEW
 
 @dataclass
-class CcdCharacteristics:
+class ChipPoint:
+    ix: int
+    iy: int
+    
+    @classmethod
+    def loads(cls, buffer):
+        buffer_len = 4
+        assert len(buffer)>=buffer_len
+        return cls(*struct.unpack("<HH", buffer[:buffer_len]))
+
+    def dumps(self):
+        return struct.pack("<HH", self.ix, self.iy)
+
+@dataclass
+class ChipBadPoint:
+    spt: ChipPoint
+    sptreplace1:ChipPoint
+    sptreplace2: ChipPoint
+    itreatment :int
+
+    @classmethod
+    def loads(cls, buffer):
+        buffer_len = 14
+        assert len(buffer)>=buffer_len
+        return cls(ChipPoint(buffer[:4]),
+                   ChipPoint(buffer[4:8])
+                   ChipPoint(buffer[8:12])
+                   struct.unpack("<H", buffer[12:buffer_len]))
+
+    def dumps(self):
+        return spt.dumps() + \
+            sptreplace1.dumps() + \
+            sptreplace2.dumps() + \
+            struct.pack("<H", self.itreatment)
+
+@dataclass
+class ChipBadRow:
+    """ROW STARTX ENDX Y"""    
+    sptstart: ChipPoint
+    sptend: ChipPoint
+    sptstartreplace:ChipPoint
+    sptendreplace: ChipPoint
+    itreatment :int
+
+    @classmethod
+    def loads(cls, buffer):
+        buffer_len = 18
+        assert len(buffer)>=buffer_len
+        return cls(ChipPoint(buffer[:4]),
+                   ChipPoint(buffer[4:8]),
+                   ChipPoint(buffer[8:12]),
+                   ChipPoint(buffer[12:16]),
+                   struct.unpack("<H", buffer[16:buffer_len])[0])
+
+    def dumps(self):
+        return sptstart.dumps() + \
+            sptend.dumps() + \
+            sptstartreplace.dumps() + \
+            sptendreplace.dumps() + \
+            struct.pack("<H", self.itreatment)
+
+@dataclass
+class ChipBadColumn:
+    """COLUMN X STARTY ENDY"""    
+    sptstart: ChipPoint
+    sptend: ChipPoint
+    sptstartreplace:ChipPoint
+    sptendreplace: ChipPoint
+    itreatment :int
+    ilowlimit: int
+    ihighlimit: int
+
+    @classmethod
+    def loads(cls, buffer):
+        buffer_len = 22
+        assert len(buffer)>=buffer_len
+        return cls(ChipPoint(buffer[:4]),
+                   ChipPoint(buffer[4:8]),
+                   ChipPoint(buffer[8:12]),
+                   ChipPoint(buffer[12:16]),
+                   *struct.unpack("<HHH", buffer[16:buffer_len]))
+
+    def dumps(self):
+        return sptstart.dumps() + \
+            sptend.dumps() + \
+            sptstartreplace.dumps() + \
+            sptendreplace.dumps() + \
+            struct.pack("<HHH", self.itreatment, self.ilowlimit, self.ihighlimit)
+
+@dataclass
+class ChipBadPolygon:
+    itype:int
+    ipoints: int
+    iax: List[int]
+    iay: List[int]
+
+    @classmethod
+    def loads(cls, buffer):
+        buffer_len = 2*(2+2*CHIPCHARACTERISTICS_POLYGONTYPE.MAXPOINTS)
+        assert len(buffer)>=buffer_len
+        lst = struct.unpack("<HH"+"H"*2*CHIPCHARACTERISTICS_POLYGONTYPE.MAXPOINTS,
+                            buffer[:buffer_len])
+        return cls(lst[0], lst[1],
+                   lst[2:2+CHIPCHARACTERISTICS_POLYGONTYPE.MAXPOINTS],
+                   lst[2+CHIPCHARACTERISTICS_POLYGONTYPE.MAXPOINTS:],)
+
+    def dumps(self):
+        return struct.pack("<HH"+"H"*2*CHIPCHARACTERISTICS_POLYGONTYPE.MAXPOINTS,
+                           self.itype, self.ipoints, *self.iax, *self.iay)
+
+@dataclass
+class ChipMachineFunction:
+    """
+    xl is log10 of true Xray intensity
+    y=A * exp (B * xl)
+    """
+    iismachinefunction: int
+    da_machinefct: float
+    db_machinefct: float
+
+    @classmethod
+    def loads(cls, buffer):
+        buffer_len = 2+8+8
+        assert len(buffer)>=buffer_len
+        lst = struct.unpack("<HH"+"H"*2*CHIPCHARACTERISTICS_POLYGONTYPE.MAXPOINTS,
+                            buffer[:buffer_len])
+        return cls(lst[0], lst[1],
+                   lst[2:2+CHIPCHARACTERISTICS_POLYGONTYPE.MAXPOINTS],
+                   lst[2+CHIPCHARACTERISTICS_POLYGONTYPE.MAXPOINTS:],)
+
+    def dumps(self):
+        return struct.pack("<HH"+"H"*2*CHIPCHARACTERISTICS_POLYGONTYPE.MAXPOINTS,
+                           self.itype, self.ipoints, *self.iax, *self.iay)
+    
+@dataclass
+class CcdCharacteristiscs:
     """Names are using the hugarian notation: the first letter describes the type
     
     strings are 256bytes long
@@ -171,4 +329,4 @@ class CcdImage(FabioImage):
 
 
 # This is not compatibility with old code:
-templateimage = TemplateImage
+ccdimage = CcdImage
